a2767bbee7d5be04aceb9ef351073ace1e48a0ad
Jonathan <jonathan.031192@gmail.com>
2024-08-28 16:20:52 -0300

    Fixing scripts

--- start.sh
+++ start.sh
@@ -1,2 +1,3 @@
+#!/bin/bash
 docker image inspect coding-challenge-fullstack-frontend:latest >/dev/null 2>&1 && echo Image already created || docker build -t coding-challenge-fullstack-frontend:latest .
 docker-compose up -d
